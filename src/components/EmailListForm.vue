d645a6b545470c398b12ba8ee3dda8e2acae2496
Austin <amarchese3@gmail.com>
2020-12-03 10:19:51 -0800

    Adding Styling to form

--- src/components/EmailListForm.vue
+++ src/components/EmailListForm.vue
@@ -1,105 +1,107 @@
 <template>
-  <div class="w-full max-w-xl">
-    <h1 class="subtitle">
-      Contact Us
-    </h1>
-    <!-- TODO: Better Success handling -->
-    <form
-      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      name="contact-form"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-       @submit.prevent="handleSubmit"
-    >
-    <input type="hidden" name="form-name" value="contact-form" />
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-xl font-bold mb-2"
-          for="email"
-        >Email</label>
-        <input
-          id="email"
-          v-model="email"
-          name="email"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="email"
-          placeholder="satoshi@nakamoto.com"
-          data-hj-whitelist
-        >
-      </div>
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-xl font-bold mb-2"
-          for="name"
-        >Name</label>
-        <input
-          id="name"
-          v-model="name"
-          name="name"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Satoshi Nakamoto"
-          data-hj-whitelist
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          :disabled="!canSubmit"
-          :class="
+    <div class="w-full max-w-xl">
+        <CardFormat header="Intersted in your own site?" :quote="this.displayMessage" author="Austin">
+        <div v-if="isFormDisplayed">
+            <form  name="contact-form" method="POST"
+                data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="handleSubmit">
+                <input type="hidden" name="form-name" value="contact-form" />
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-xl font-bold mb-2" for="email">Email: </label>
+                    <input id="email" v-model="email" name="email"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="email" placeholder="satoshi@nakamoto.com" data-hj-whitelist>
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-xl font-bold mb-2" for="name">Name: </label>
+                    <input id="name" v-model="name" name="name"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text" placeholder="Satoshi Nakamoto" data-hj-whitelist>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button :disabled="!canSubmit" :class="
             `${!canSubmit &&
-              'opacity-50 cursor-not-allowed'} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full`
-          "
-          type="submit"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
+              'opacity-30 cursor-not-allowed'} bg-blue-500 hover:bg-blue-700  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full`
+          " type="submit">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+        <!-- <div v-else>
+                    <p class="lead">
+{{this.displayMessage}}
+                    </p>
+        </div> -->
+
+        </CardFormat>
+    </div>
 </template>
 <script>
-import axios from "axios"
+import CardFormat from './CardFormat.vue';
+    import axios from "axios"
 
-export default {
-  name: 'EmailListForm',
-  data () {
-    return {
-      email: '',
-      name: '',
-    }
-  },
-  computed: {
-    canSubmit () {
-      return !!(this.email && this.name)
-    }
-  },
-
- methods: {
-    encode (data) {
-      return Object.keys(data)
-        .map(
-          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-        )
-        .join("&");
+    export default {
+        name: 'EmailListForm',
+            components: {
+      CardFormat,
+      // WorldMap,
     },
-    handleSubmit () {
-      const axiosConfig = {
-        header: { "Content-Type": "application/x-www-form-urlencoded" }
-      };
-      console.log("Ye")
-      axios.post(
-        "/",
-        this.encode({
-          "form-name": "contact-form",
-          "email": this.email,
-          "name": this.name,
-        }),
-        axiosConfig
-      );
-    }
-  }
+      
+        data() {
+            return {
+                isFormDisplayed: true,
+                email: '',
+                name: '',
+                displayMessage: "Fill out the below form and I'll reach out to you!"
+            }
+        },
+        computed: {
+            canSubmit() {
+                return !!(this.email && this.name)
+            }
+        },
 
-}
+        methods: {
+            encode(data) {
+                return Object.keys(data)
+                    .map(
+                        key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+                    )
+                    .join("&");
+            },
+            handleSubmit() {
+                const axiosConfig = {
+                    header: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    }
+                };
+                axios.post(
+                        "/",
+                        this.encode({
+                            "form-name": "contact-form",
+                            "email": this.email,
+                            "name": this.name,
+                        }),
+                        axiosConfig
+                    ).then(() => {
+                        console.log("Success")
+                        this.isFormDisplayed = false
+                        this.displayMessage =
+                            "Thank You For Submitting the form, I look forward to discussing the next steps!"
+                    })
+                    .catch(() => {
+                        console.log("Error")
+                        this.isFormDisplayed = true
+                        this.displayMessage =
+                            "There was an error with your Submission. Please retry and if error continues, DM me @Macncheeasy on Instagram! "
+                    })
+            }
+        }
+    }
 </script>
-<style></style>
+<style>
+    .subtitle {
+
+        font-size: "40px" !important
+    }
+</style>
